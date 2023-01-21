85f20f5888e59d34b56d640f0f8729919e14f73c
Maxim <334890@gmail.com>
2023-01-21 10:50:19 +1000

    Add js password comparison

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //
+//const submitBtn = document.querySelector("#submit");
+const form = document.querySelector("#sign-up");
+const password1 = document.querySelector("#password");
+const password2 = document.querySelector("#password-repeat");
+
+form.addEventListener("submit", comparePasswords);
+
+function comparePasswords(event) {
+  if (password1.value !== password2.value) {
+    event.preventDefault();
+    password1.classList.add("error");
+    password2.classList.add("error");
+    console.log("wrongo");
+  } else {
+    password1.classList.remove("error");
+    password2.classList.remove("error");
+    console.log("submitting");
+  }
+}
